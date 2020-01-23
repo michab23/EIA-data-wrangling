81e6cdfcd18969a33fd222493e748530b4a546dd
michaelb <michaelb@cio.gov.il>
2020-01-23 13:23:07 +0200

    add some date format for results

--- eai_data_wrangling.py
+++ eai_data_wrangling.py
@@ -1,11 +1,12 @@
 import requests
 import csv
+import datetime
 
 daily = 'https://api.eia.gov/series/?api_key=4d7348d17faec3ce8bf2af9c662ec140&series_id=NG.RNGWHHD.D'
 monthly = 'https://api.eia.gov/series/?api_key=4d7348d17faec3ce8bf2af9c662ec140&series_id=NG.RNGWHHD.M'
 
 
-def generate_csv(period_type, report_type):
+def generate_csv(period_type, report_type, format):
     # get  data
     response = requests.get(period_type)
     parsed_data = response.json()['series'][0]['data']
@@ -15,15 +16,17 @@ def generate_csv(period_type, report_type):
     csvwriter = csv.writer(price_report)
     # write headers
     csvwriter.writerow(["Date", "Price"])
+
     for emp in parsed_data:
         # write rows
-        csvwriter.writerow([emp[0],
+
+        csvwriter.writerow([datetime.datetime.strptime(emp[0], format).date(),
                             emp[1]])
     price_report.close()
 
 
-generate_csv(daily, 'daily_report')
-generate_csv(monthly, 'monthly_report')
+generate_csv(daily, 'daily_report',  "%Y%m%d")
+generate_csv(monthly, 'monthly_report', "%Y%m")
 
 
 
